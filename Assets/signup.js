8f5b56b6071f216b88028d75be0702e4cc97aaaf
Kipkorir2017 <pkobenj41@gmail.com>
2021-06-23 19:19:40 +0300

    added other.html and other.css files

--- Assets/signup.js
+++ Assets/signup.js
@@ -1,14 +1,31 @@
 function validateForm() {
     var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value.indexOf("@");
     var password = document.getElementById("password").value;
-    var email = document.getElementById("email").value;
     var location =document.getElementById("location").value;
+    submitOK = "true";
 
-    if (name == ""|| password =="" ||location==""||email =="") {
-      alert("Filled out this Field");
-      return false;
-    }
-    // else{
-    //     if("name >3 &&<35")
+    // if (name == ""|| password =="" ||location==""||email =="") {
+    //   alert("Filled out this Field");
+    //   return false;
     // }
+    if (name.length > 8) {
+        alert("The name may have no more than 8 characters");
+        submitOK = "false";
+      }
+      if ( email== -1) {
+        alert("Not a valid e-mail!");
+        submitOK = "false";
+      }
+      if (password> 6) {
+        alert("password must be more than 6 characters");
+        submitOK = "false";
+}
+if (location.length <3) {
+    alert("invalid location");
+    submitOK = "false";
+  }
+if (submitOK == "false") {
+    return false;
+  }
 }
